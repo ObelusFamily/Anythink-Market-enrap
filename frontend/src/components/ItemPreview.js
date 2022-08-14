ddc86ba8e15c5a05b3916b4592693cdd654cfa2c
Christian Sánchez <chsanch@pm.me>
2022-08-14 12:14:24 +0200

    fix broken image

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,6 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
+  const imageToDisplay = item.image ? item.image : '/placeholder.png'
 
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -36,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={imageToDisplay}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
